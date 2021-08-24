b82372619b22856882b76baa07521894d51e23b2
Adir Sason <adirx20@gmail.com>
2021-08-24 18:31:08 +0300

    start js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+// opening and closing the popup --->
+let popup = document.querySelector('.popup');
+
+function popupOpen() {
+    popup.classList.add('popup_opened');
+}
+
+function popupClose() {
+    popup.classList.remove('popup_opened');
+}
+
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close-button');
+editButton.addEventListener('click', popupOpen);
+closeButton.addEventListener('click', popupClose);
+//
+
+// submitting the form --->
 let formElement = document.querySelector('.form');
 
 console.log(formElement.classList);
@@ -9,6 +27,13 @@ function handleFormSubmit(evt) {
     let jobInput = formElement.querySelector('.form__input_type_profession');
 }
 
-let popup = document.querySelector('.popup');
+handleFormSubmit();
 
-popup.classList.add('popup_opened');
+let name = document.querySelector('.profile__name');
+let job = document.querySelector('.profile__profession');
+let saveButton = document.querySelector('.form__save-button');
+
+// let likeButton = document.querySelectorAll('.element__like-button');
+// for (i = 0; i < likeButton.length; i++) {
+//     likeButton[i].addEventListener('click', addClass);
+// }
