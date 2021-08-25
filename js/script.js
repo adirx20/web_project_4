687d961d0263a79ae7e2ce0ed7cb82c6e758fc01
Adir Sason <adirx20@gmail.com>
2021-08-25 17:18:50 +0300

    fix res & js

--- js/script.js
+++ js/script.js
@@ -1,41 +1,38 @@
 // opening and closing the popup --->
 let popup = document.querySelector('.popup');
-
-function popupOpen(evt) {
-    evt.preventDefault();
-
-    popup.classList.add('popup_opened');
-}
-
-function popupClose(evt) {
-    evt.preventDefault();
-
-    popup.classList.remove('popup_opened');
-}
-
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
-editButton.addEventListener('click', popupOpen);
-closeButton.addEventListener('click', popupClose);
-//
-
-// submitting the form --->
 let formElement = document.querySelector('.form');
 let saveButton = document.querySelector('.form__save-button');
 
+function popupOpen() {
+    let name = document.querySelector('.profile__name');
+    let job = document.querySelector('.profile__profession');
+    
+    popup.classList.add('popup_opened');
+}
+
+function popupClose() {
+    popup.classList.remove('popup_opened');
+}
+//
+
+// submitting the form --->
 function handleFormSubmit(evt) {
     evt.preventDefault();
-
+    
     let nameInput = formElement.querySelector('.form__input_type_name');
     let jobInput = formElement.querySelector('.form__input_type_profession');
     let name = document.querySelector('.profile__name');
     let job = document.querySelector('.profile__profession');
-
+    
     name.textContent = nameInput.value;
     job.textContent = jobInput.value;
-    popup.classList.remove('popup__opened');
+    popup.classList.remove('popup_opened');
 }
 
+editButton.addEventListener('click', popupOpen);
+closeButton.addEventListener('click', popupClose);
 formElement.addEventListener('submit', handleFormSubmit);
 //
 
