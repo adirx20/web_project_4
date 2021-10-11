8d84d85cefdacaf923791d4259d0f6d5954a2dae
Adir Sason <adirx20@gmail.com>
2021-10-11 19:19:11 +0300

    class popup

---
+++ scripts/Popup.js
@@ -0,0 +1,25 @@
+class Popup {
+    constructor(popupSelector) {
+        this._popupElement = document.querySelector(popupSelector);
+    }
+
+    _handleEscClose = (evt) => {
+        if (evt.key === 'Escape') {
+            this.close();
+        }
+    }
+
+    open = () => {
+        this._popupElement.classList.add('popup_opened');
+        document.addEventListener('keyup', this._handleEscClose);
+    }
+
+    close = () => {
+        this._popupElement.classList.remove('popup_opened');
+        document.removeEventListener('keyup', this._handleEscClose);
+    }
+
+    setEventListeners = () => {
+        this._popupElement.querySelector('.popup__close-button').addEventListener('click', this.close);
+    }
+}
