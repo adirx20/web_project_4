ce8fb00dfa0caa7d4e3eb0d1df526db457eec310
Adir Sason <adirx20@gmail.com>
2021-11-08 20:13:08 +0200

    need fix

---
+++ src/index.js
@@ -0,0 +1,133 @@
+import './styles/index.css';
+import FormValidator from '../scripts/FormValidator.js';
+import { Card } from '../scripts/Card.js';
+import { PopupWithImage } from '../scripts/PopupWithImage.js';
+import { PopupWithForm } from '../scripts/PopupWithForm.js';
+import { UserInfo } from '../scripts/UserInfo.js';
+import { Section } from '../scripts/Section.js';
+
+// =====>
+// SETTINGS
+const settings = {
+  inputSelector: '.form__input',
+  submitButtonSelector: '.form__save-button',
+  inactiveButtonClass: 'form__save-button_disabled',
+  inputErrorClass: 'form__input_theme_error',
+  errorClass: "popup__error_visible"
+};
+
+// CARDS
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg"
+  }
+];
+
+// SELECTORS
+const editProfileModalSelector = document.querySelector('.popup_type_edit-profile');
+const addCardModalSelector = document.querySelector('.popup_type_add-card');
+const cardTemplateSelector = '.card-template';
+
+// OPEN BUTTONS
+const editProfileButton = document.querySelector('.profile__edit-button');
+const addCardButton = document.querySelector('.profile__add-button');
+
+// INPUTS 
+const profileNameInput = document.querySelector('.form__input_type_name');
+const profileJobInput = document.querySelector('.form__input_type_profession');
+
+// FORMS
+const editProfileForm = editProfileModalSelector.querySelector('.form');
+const addCardForm = addCardModalSelector.querySelector('.form');
+
+// CREATE AND RENDER CARD
+const createCard = (data) => {
+  const card = new Card(data, cardTemplateSelector, (text, link) => {
+    imageModal.open(text, link);
+  });
+  return card.getCardElement();
+}
+
+// VALIDATORS
+const editProfileFormValidator = new FormValidator(settings, editProfileForm);
+const addCardFormValidator = new FormValidator(settings, addCardForm);
+
+// CALL FORMS VALIDATON
+editProfileFormValidator.enableValidation();
+addCardFormValidator.enableValidation();
+
+// USER INFO
+const userInfo = new UserInfo({
+  profileNameSelector: '.profile__name',
+  profileJobSelector: '.profile__profession'
+});
+
+// SECTION
+const section = new Section({
+  items: initialCards,
+  renderer: (data) => {
+    const card = createCard(data);
+    section.addItem(card);
+  }
+}, '.elements');
+
+section.render();
+
+// MODALS
+const imageModal = new PopupWithImage('.popup_type_image');
+imageModal.setEventListeners();
+
+const editProfileModal = new PopupWithForm('.popup_type_edit-profile', data => userInfo.setUserInfo(data));
+editProfileModal.setEventListeners();
+
+const addCardModal = new PopupWithForm('.popup_type_add-card', (data) => {
+  const card = createCard({
+    name: data['card-title'],
+    link: data['card-link']
+  });
+  section.addItem(card);
+});
+addCardModal.setEventListeners();
+
+// OPEN BUTTONS EVENT LISTENERS
+// EDIT PROFILE
+editProfileButton.addEventListener('click', () => {
+  editProfileFormValidator.resetValidation();
+  const userData = userInfo.getUserInfo();
+
+  profileNameInput.value = userData.name;
+  profileJobInput.value = userData.job;
+
+  editProfileModal.open();
+});
+
+// ADD CARD
+addCardButton.addEventListener('click', () => {
+  addCardForm.reset();
+
+  addCardFormValidator.resetValidation();
+
+  addCardModal.open();
+});
+// <=====
